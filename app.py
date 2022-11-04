59210cbdac5565aff1f9da971442b373b794ef3e
Ahoura Mortazavi <ahoura.mortazavi@gmail.com>
2022-11-04 11:39:00 -0500

    Initial Commit

--- app.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for i in range(9,60):
-    d = str(i) + ' days ago'
-    with open("file.txt", 'a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="' + d + '" -m "Update LeetCode"')
