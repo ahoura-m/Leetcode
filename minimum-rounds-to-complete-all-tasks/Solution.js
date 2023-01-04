00305ae30d1d736feb761808d3fd31b750705f64
Ahoura Mortazavi <ahoura.mortazavi@gmail.com>
2023-01-04 10:36:16 -0600

    Leetcode Update

---
+++ minimum-rounds-to-complete-all-tasks/Solution.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} tasks
+ * @return {number}
+ */
+var minimumRounds = function (tasks) {
+    tasks.sort((a, b) => a - b)
+    console.log(tasks)
+    let currentDifficulty = tasks[0]
+    let currentCnt = 1
+    let output = 0
+    for (let i = 1; i < tasks.length; i++) {
+        if (tasks[i] == currentDifficulty) currentCnt++
+        else {
+            if (currentCnt < 2) return -1
+            if (currentCnt % 3 === 0) output += currentCnt / 3
+            else if (currentCnt % 3 === 2)
+                output += Math.floor(currentCnt / 3) + 1
+            else {
+                output += Math.floor(currentCnt / 3) + 1
+            }
+            currentDifficulty = tasks[i]
+            currentCnt = 1
+        }
+    }
+    if (currentCnt < 2) return -1
+    if (currentCnt % 3 === 0) output += currentCnt / 3
+    else if (currentCnt % 3 === 2) output += Math.floor(currentCnt / 3) + 1
+    else {
+        output += Math.floor(currentCnt / 3) + 1
+    }
+    return output
+}
+
+console.log(minimumRounds([2, 3, 3]))
